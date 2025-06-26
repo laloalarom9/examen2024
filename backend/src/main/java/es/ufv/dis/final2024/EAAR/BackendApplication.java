e4de38a472cf8b0743c7d314ab7efc423112dae5
Eduardo <9207448@alumnos.ufv.es>
2025-06-26 14:06:31 +0200

    Vaadin y springboot añadidos

---
+++ backend/src/main/java/es/ufv/dis/final2024/EAAR/BackendApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final2024.EAAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
